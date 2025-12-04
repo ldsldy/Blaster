d52d669cfd30e7d95bfaa334de193fd4bc396432
LDY <137361731+ldsldy@users.noreply.github.com>
2025-12-04 18:00:54 +0900

    캐릭터 이동 구현, 애니메이션 구현 중

--- Source/Blaster/Character/BlasterCharacter.cpp
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -4,6 +4,10 @@
 #include "BlasterCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputAction.h"
+
 // Sets default values
 ABlasterCharacter::ABlasterCharacter()
 {
@@ -23,6 +27,42 @@ void ABlasterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem
+			= ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(PlayerMappingContext, 0);
+		}
+	}
+}
+
+void ABlasterCharacter::Move(const FInputActionValue& Value)
+{
+	if (Controller != nullptr)
+	{
+		FVector2D MovementVector = Value.Get<FVector2D>();
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+
+		// 컴퓨터 그래픽스 변환 행렬에 대한 지식 필요
+		const FVector ForwardDirection(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X));
+		const FVector RightDirection(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y));
+
+		// 언리얼은 Y축이 앞뒤 이동, X축이 좌우 이동
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void ABlasterCharacter::Look(const FInputActionValue& Value)
+{
+	if (Controller != nullptr)
+	{
+		FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+		AddControllerPitchInput(LookAxisVector.Y);
+		AddControllerYawInput(LookAxisVector.X);
+	}
 }
 
 void ABlasterCharacter::Tick(float DeltaTime)
@@ -35,5 +75,11 @@ void ABlasterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABlasterCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABlasterCharacter::Look);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::Jump);
+	}
 }
 
--- Source/Blaster/Character/BlasterCharacter.h
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "BlasterCharacter.generated.h"
 
 UCLASS()
@@ -19,10 +20,29 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	//
+	// 입력 액션 함수
+	//
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+protected:
+	//
+	// 매핑 컨텍스트 및 입력 액션
+	//
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input");
+	class UInputMappingContext* PlayerMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input");
+	class UInputAction* MoveAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input");
+	class UInputAction* LookAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input");
+	class UInputAction* JumpAction;
+
 private:
-	UPROPERTY(VisibleAnywhere, Category = Camera)
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
 	TObjectPtr<class USpringArmComponent> CameraBoom;
-	UPROPERTY(VisibleAnywhere, Category = Camera)
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
 	TObjectPtr<class UCameraComponent> FollowCamera;
 
 public:
